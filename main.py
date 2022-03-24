715ebaa85b6cd441e86ae07b31447d718f59bde1
ilia <iliaman1993@gmail.com>
2022-03-24 18:27:32 +0300

    add some cal_cool

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
-operations = ('+', ':', '**', '-', '*')
+
 app = Flask(__name__)
 
 
-@app.route('/<int:a><oper><int:b>/')
-def calculate(a, b, oper):
-    if oper in operations and oper == '+':
-        return str(int(a) + int(b))
-    elif oper in operations and oper == ':':
-        return str(int(a) / int(b))
-    elif oper in operations and oper == '**':
-        return str(int(a) ** int(b))
-    elif oper in operations and oper == '-':
-        return str(int(a) - int(b))
-    elif oper in operations and oper == '*':
-        return str(int(a) * int(b))
+@app.route('/')
+@app.route('/index/')
+def index():
+    return render_template('index.html', name='Ilia', title='Home page')
+
+
+@app.route('/<float:num>/')
+def x2(num):
+    return render_template('index.html', number=f"{float(num)}", text=f"Ваше число {float(num)}, умноженное на 2: {float(num)*2}")
+
+# @app.route('/<int:a><oper><int:b>/')
+# def calculate(a, b, oper):
+#     operations = ('+', ':', '**', '-', '*')
+#     if oper in operations and oper == '+':
+#         return str(int(a) + int(b))
+#     elif oper in operations and oper == ':':
+#         return str(int(a) / int(b))
+#     elif oper in operations and oper == '**':
+#         return str(int(a) ** int(b))
+#     elif oper in operations and oper == '-':
+#         return str(int(a) - int(b))
+#     elif oper in operations and oper == '*':
+#         return str(int(a) * int(b))
+
 
 
 if __name__ == '__main__':
